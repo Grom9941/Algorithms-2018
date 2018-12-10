7a07a83eb71ce515abfa0a028f36b59f76e5277a
Grom9941 <skv.41@ya.ru>
2018-12-11 00:09:35 +0300

    lesson 6(две задачи)(подкоректировать код)

--- src/lesson6/JavaDynamicTasks.java
+++ src/lesson6/JavaDynamicTasks.java
@@ -2,6 +2,10 @@ package lesson6;
 
 import kotlin.NotImplementedError;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @SuppressWarnings("unused")
@@ -9,7 +13,7 @@ public class JavaDynamicTasks {
     /**
      * Наибольшая общая подпоследовательность.
      * Средняя
-     *
+     * <p>
      * Дано две строки, например "nematode knowledge" и "empty bottle".
      * Найти их самую длинную общую подпоследовательность -- в примере это "emt ole".
      * Подпоследовательность отличается от подстроки тем, что её символы не обязаны идти подряд
@@ -18,13 +22,37 @@ public class JavaDynamicTasks {
      * При сравнении подстрок, регистр символов *имеет* значение.
      */
     public static String longestCommonSubSequence(String first, String second) {
-        throw new NotImplementedError();
+        int[][] lengths = new int[first.length() + 1][second.length() + 1];
+
+        for (int i = 1; i < first.length() + 1; i++) {
+            for (int j = 1; j < second.length() + 1; j++) {
+                if (first.charAt(i - 1) == second.charAt(j - 1))
+                    lengths[i][j] = lengths[i - 1][j - 1] + 1;
+                else
+                    lengths[i][j] = Math.max(lengths[i][j - 1], lengths[i - 1][j]);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int x = first.length(), y = second.length();
+             x != 0 && y != 0; ) {
+            if (first.charAt(x - 1) == second.charAt(y - 1)) {
+                sb.append(first.charAt(x - 1));
+                x--;
+                y--;
+            } else if (lengths[x][y - 1] > lengths[x - 1][y]) {
+                y--;
+            } else if (lengths[x][y - 1] < lengths[x - 1][y]) {
+                x--;
+            } else x--;
+        }
+        return sb.reverse().toString();
     }
 
     /**
      * Наибольшая возрастающая подпоследовательность
      * Средняя
-     *
+     * <p>
      * Дан список целых чисел, например, [2 8 5 9 12 6].
      * Найти в нём самую длинную возрастающую подпоследовательность.
      * Элементы подпоследовательности не обязаны идти подряд,
@@ -40,25 +68,64 @@ public class JavaDynamicTasks {
     /**
      * Самый короткий маршрут на прямоугольном поле.
      * Сложная
-     *
+     * <p>
      * В файле с именем inputName задано прямоугольное поле:
-     *
+     * <p>
      * 0 2 3 2 4 1
      * 1 5 3 4 6 2
      * 2 6 2 5 1 3
      * 1 4 3 2 6 2
      * 4 2 3 1 5 0
-     *
+     * <p>
      * Можно совершать шаги длиной в одну клетку вправо, вниз или по диагонали вправо-вниз.
      * В каждой клетке записано некоторое натуральное число или нуль.
      * Необходимо попасть из верхней левой клетки в правую нижнюю.
      * Вес маршрута вычисляется как сумма чисел со всех посещенных клеток.
      * Необходимо найти маршрут с минимальным весом и вернуть этот минимальный вес.
-     *
+     * <p>
      * Здесь ответ 2 + 3 + 4 + 1 + 2 = 12
      */
-    public static int shortestPathOnField(String inputName) {
-        throw new NotImplementedError();
+    public static int shortestPathOnField(String inputName) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new FileReader(inputName));
+
+        List<String> array = new ArrayList<>();
+        int numberLine = 0;
+        String line;
+
+        while ((line=reader.readLine()) != null) {
+            numberLine++;
+            array.add(line);
+        }
+        reader.close();
+
+        int numberWeigth = array.get(0).split(" ").length;
+        int[][] matrix = new int[numberLine][numberWeigth];
+
+        numberLine=0;
+        for (String str: array) {
+                for (int i = 0; i < str.split(" ").length; i++) {
+                    matrix[numberLine][i] = str.charAt(2 * i) - 48;
+                }
+                numberLine++;
+        }
+
+        int numberTop;int numberLeft;int numberDiagonal;
+        byte trueAll;
+
+        for (int i=0;i<numberLine;i++){
+            for (int j=0;j<numberWeigth;j++){
+                trueAll=0;
+                if (i-1>=0) numberTop= matrix[i-1][j]; else {numberTop=Integer.MAX_VALUE;trueAll++;}
+                if (j-1>=0) numberLeft= matrix[i][j-1]; else {numberLeft=Integer.MAX_VALUE;trueAll++;}
+                if (i-1>=0 && j-1>=0) numberDiagonal= matrix[i-1][j-1]; else {numberDiagonal=Integer.MAX_VALUE;trueAll++;}
+
+                if (trueAll!=3) {
+                    matrix[i][j] = matrix[i][j] + Math.min(Math.min(numberTop, numberLeft), numberDiagonal);
+                }
+            }
+        }
+        return matrix[numberLine-1][numberWeigth-1];
     }
 
     // Задачу "Максимальное независимое множество вершин в графе без циклов"
--- test/lesson6/AbstractDynamicTests.kt
+++ test/lesson6/AbstractDynamicTests.kt
@@ -9,26 +9,20 @@ abstract class AbstractDynamicTests {
         assertEquals("13", longestCommonSubSequence("123", "13"))
         assertEquals("здс", longestCommonSubSequence("здравствуй мир", "мы здесь"))
         assertEquals("emt ole", longestCommonSubSequence("nematode knowledge", "empty bottle"))
-        assertEquals("e kerwelkkd r", longestCommonSubSequence(
-                "oiweijgw kejrhwejelkrw kjhdkfjs hrk",
-                "perhkhk lerkerorwetp lkjklvvd durltr"
-        ))
+        assertEquals("e kerwelkkd r", longestCommonSubSequence("oiweijgw kejrhwejelkrw kjhdkfjs hrk", "perhkhk lerkerorwetp lkjklvvd durltr"))
         assertEquals(""" дд саы чтых,
 евшнео ваа се сви дн.
-        """.trimIndent(), longestCommonSubSequence(
-                """
+        """.trimIndent(), longestCommonSubSequence("""
 Мой дядя самых честных правил,
 Когда не в шутку занемог,
 Он уважать себя заставил
 И лучше выдумать не мог.
-                """.trimIndent(),
-                """
+                """.trimIndent(), """
 Так думал молодой повеса,
 Летя в пыли на почтовых,
 Всевышней волею Зевеса
 Наследник всех своих родных.
-                """.trimIndent()
-        ))
+                """.trimIndent()))
     }
 
     fun longestIncreasingSubSequence(longestIncreasingSubSequence: (List<Int>) -> List<Int>) {
@@ -36,14 +30,9 @@ abstract class AbstractDynamicTests {
         assertEquals(listOf(1), longestIncreasingSubSequence(listOf(1)))
         assertEquals(listOf(1, 2), longestIncreasingSubSequence(listOf(1, 2)))
         assertEquals(listOf(2), longestIncreasingSubSequence(listOf(2, 1)))
-        assertEquals(listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
-                longestIncreasingSubSequence(listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
-        )
+        assertEquals(listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), longestIncreasingSubSequence(listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)))
         assertEquals(listOf(2, 8, 9, 12), longestIncreasingSubSequence(listOf(2, 8, 5, 9, 12, 6)))
-        assertEquals(listOf(23, 34, 56, 87, 91, 98, 140, 349), longestIncreasingSubSequence(listOf(
-                23, 76, 34, 93, 123, 21, 56, 87, 91, 12, 45, 98, 140, 12, 5, 38, 349, 65, 94,
-                45, 76, 15, 99, 100, 88, 84, 35, 88
-        )))
+        assertEquals(listOf(23, 34, 56, 87, 91, 98, 140, 349), longestIncreasingSubSequence(listOf(23, 76, 34, 93, 123, 21, 56, 87, 91, 12, 45, 98, 140, 12, 5, 38, 349, 65, 94, 45, 76, 15, 99, 100, 88, 84, 35, 88)))
     }
 
     fun shortestPathOnField(shortestPathOnField: (String) -> Int) {
